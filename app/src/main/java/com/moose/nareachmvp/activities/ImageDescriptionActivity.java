a01eeff09ac91de3dfb7e1b5bf524936266187f1
yangshunfa <yang2time@163.com>
2016-09-29 14:52:40 +0800

    图片显示详情Activity

---
+++ app/src/main/java/com/moose/nareachmvp/activities/ImageDescriptionActivity.java
@@ -0,0 +1,15 @@
+package com.moose.nareachmvp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.moose.nareachmvp.R;
+
+public class ImageDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_description);
+    }
+}
